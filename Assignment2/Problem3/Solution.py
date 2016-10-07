ce95e283875ba3b9ed1b9fb540c0417ed857c84e
Aditya <mahajanaditya007@gmail.com>
2016-10-07 09:19:08 -0500

    made the modifications to the HMM

--- Assignment2/Problem3/Solution.py
+++ Assignment2/Problem3/Solution.py
@@ -1,6 +1,11 @@
 from __future__ import division
+
+#assuming that the probability of transition
+#from any state to final state is 1
 def constructHMM(o):
     T=len(o)
+    best_score=0
+    backtrace_start=None
     states=2    #Hot-1,Cold-2,Start-0
     a = [[0 for x in range(states+1)] for x in range(states+1)]
     a[0][0] = 0
@@ -11,9 +16,9 @@ def constructHMM(o):
     a[2][1] = 0.4
     a[2][2] = 0.6
 
-    #outcome variables are of three type 0,1 and 2
+    #outcome variables are of three type 1,2 and 3
     #b[i][j] is the probability of seeing output j given the state i
-    b=[[0 for i in range(T+1)] for j in range(states+1)]
+    b=[[0 for i in range(T)] for j in range(states+1)]
     b[1][1]=0.2
     b[1][2]=0.4
     b[1][3]=0.4
@@ -21,20 +26,38 @@ def constructHMM(o):
     b[2][2]=0.4
     b[2][3]=0.1
 
-    backtrack=[[0 for i in range(states+1)] for j in range(T+1)]
-    v=[[0 for i in range(states+1)] for j in range(T+1)]
+    backtrack=[[0 for i in range(states+1)] for j in range(T)]
+    v=[[0 for i in range(states+1)] for j in range(T)]
 
     #initialization
     for j in range(1,states+1):
         v[1][j]=a[0][j]*b[j][o[1]]
         backtrack[1][j]=0
 
-    for t in range(2,T+1):
+    for t in range(2,T):
         for j in range(1,states+1):
             for i in range(1,states+1):
                 if(v[t-1][i]*a[i][j]*b[j][o[t]]>v[t][j]):
                     backtrack[t][j]=i #argmax
                 v[t][j]=max(v[t-1][i]*a[i][j]*b[j][o[t]],v[t][j])
+                print v[t][j]
+
+
+    for i in range(1,states+1):
+        if best_score<v[T-1][i]:
+            best_score=v[T-1][i]
+            backtrace_start=i
+
+    return best_score,backtrace_start
+
+
+if __name__=='__main__':
+    o=map(int,raw_input().split())
+    o=[0]+o
+    print constructHMM(o)
+
+
+
 
 
 
