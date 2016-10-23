ebe0544d7ca78e758c75e3621e597496bfca6557
Aditya <mahajanaditya007@gmail.com>
2016-10-23 12:28:50 -0500

    updating the solution

--- Assignment3/solution.py
+++ Assignment3/solution.py
@@ -4,20 +4,21 @@ import sys
 
 
 def computerOverlap( synset, sentence ):
-    gloss = set(TreebankWordTokenizer().tokenize(synset.definition()))
+    meaning_set = set(TreebankWordTokenizer().tokenize(synset.definition()))
     sentence = set(sentence.split(" "))
-    return len( gloss.intersection(sentence) )
+    return len( meaning_set.intersection(sentence) ) #get the intersection between the meaning and the sentence
 
 def simplifiedLesk( word, sentence ):
     best_sense = None
     max_overlap = 0
+    #get the base word for the inflected words
     word=wordnet.morphy(word) if wordnet.morphy(word) is not None else word
     for sense in wordnet.synsets(word):
         overlap = computerOverlap(sense,sentence)
-        signature=sense.hyponyms()
-        for h in signature:
-            overlap += computerOverlap( h, sentence )
-        if overlap > max_overlap:
+        examples=sense.hyponyms() #computing the hyponyms of the words
+        for h in examples:
+            overlap += computerOverlap( h, sentence )#adding the overlap of the hyponyms of the words
+        if overlap > max_overlap: #get the best overlap
                 max_overlap = overlap
                 best_sense = sense
     return best_sense
@@ -27,5 +28,6 @@ sentence = raw_input("Enter the Sentence :")
 word = raw_input("Enter the word :")
 
 res = simplifiedLesk(word,sentence)
-print "\n\nSynset:",res
+print '\n'
+print "Synset:",res
 print "Meaning:",res.definition()
